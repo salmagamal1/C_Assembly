fda26f2b6063ca61ef87708d443d52314e018aff
Aya Ehab Ramadan <124000654+AyA-EhaB@users.noreply.github.com>
2024-12-12 21:07:02 +0200

    Update Even_counter.c

--- Even_counter.c
+++ Even_counter.c
@@ -4,11 +4,9 @@
 #include <stdio.h>
 
 int main(){
-  int arr[10];
+  int arr[10] = {10, 31, 5, 7, 11, 3, 8, 40, 12, 4};
   int counter = 0;
-  for (int i = 0 ; i < 10 ; i++){
-    scanf("%d",&arr[i]);
-  }
+
   for (int i = 0 ;i<10;i++){
     if (arr[i] % 2 == 0){
       counter++;
